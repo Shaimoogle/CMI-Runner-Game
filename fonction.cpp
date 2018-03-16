77e19f113f04c10c6de544a426e8f58eceead004
caljakare <37236916+caljakare@users.noreply.github.com>
2018-03-16 14:27:05 +0100

    test

---
+++ fonction.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+
+void start(){
+  float jo, en ;
+  jo = 0;
+  en = 100;  
+}
+
+void lerp(float &a, float v){
+  a=a-v;
+}
+
+bool defeatj(float a){
+  return a==0;
+}
+
+bool win(float a, int b){
+  int t2[3]={1,2,0};
+  for (int i=0; i<3; i++){
+    return t2[i]==b;
+}
+  
+
+
+
+int main(){
+  return 0;
+}
